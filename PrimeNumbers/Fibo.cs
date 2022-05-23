39cca2d700da283922b707c3ab7c3545cd27c4ed
Bimka09 <90Alex09@mail.ru>
2022-05-23 22:15:42 +0300

    Добавьте файлы проекта.

---
+++ PrimeNumbers/Fibo.cs
@@ -0,0 +1,48 @@
+﻿namespace PrimeNumbers
+{
+    public class Fibo
+    {
+        private int[,] _baseMatrix {get;set;}
+        private int[,] _resultMatrix { get; set; }
+
+        public void Multiply(bool checker)
+        {
+            int a11;
+            int a12;
+            int a21;
+            int a22;
+            if (checker)
+            {
+                a11 = _resultMatrix[0, 0] * _baseMatrix[0, 0] + _resultMatrix[0, 1] * _baseMatrix[1, 0];
+                a12 = _resultMatrix[0, 0] * _baseMatrix[0, 1] + _resultMatrix[0, 1] * _baseMatrix[1, 1];
+                a21 = _resultMatrix[1, 0] * _baseMatrix[0, 0] + _resultMatrix[1, 1] * _baseMatrix[1, 0];
+                a22 = _resultMatrix[1, 0] * _baseMatrix[0, 1] + _resultMatrix[1, 1] * _baseMatrix[1, 1];
+                _resultMatrix = new int[,] { { a11, a12 }, { a21, a22 } };
+            }
+            else
+            {
+                a11 = _baseMatrix[0, 0] * _baseMatrix[0, 0] + _baseMatrix[0, 1] * _baseMatrix[1, 0];
+                a12 = _baseMatrix[0, 0] * _baseMatrix[0, 1] + _baseMatrix[0, 1] * _baseMatrix[1, 1];
+                a21 = _baseMatrix[1, 0] * _baseMatrix[0, 0] + _baseMatrix[1, 1] * _baseMatrix[1, 0];
+                a22 = _baseMatrix[1, 0] * _baseMatrix[0, 1] + _baseMatrix[1, 1] * _baseMatrix[1, 1];
+                _baseMatrix = new int[,] { { a11, a12 }, { a21, a22 } };
+            }
+        }
+        public long PowerLogN(int position)
+        {
+            _baseMatrix = new int[,] { { 1, 1 }, { 1, 0 } };
+            _resultMatrix = new int[,] { { 1, 0 }, { 0, 1 } };
+
+            for (; position > 1; position /= 2)
+            {
+                if (position % 2 == 1)
+                {
+                    Multiply(true);
+                }
+                Multiply(false);
+            }
+            Multiply(true);
+            return _resultMatrix[1,1];
+        }
+    }
+}
---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(PowerN_2LogN(3, 5));
+            //Console.WriteLine(PowerLogN(3, 5));
+
+            var fibo = new Fibo();
+            Console.WriteLine(fibo.PowerLogN(24));
+        }
+        static long PowerN_2LogN(long a, long n)
+        {
+            bool firstCheck = true;
+            long a1 = a;
+            int j = 2;
+            long b = n / 2;
+            for (;j <= b; j *= 2)
+            {
+                if (firstCheck)
+                {
+                    a *= a;
+                    firstCheck = false;
+                }
+                a *= a;
+            }
+            for (; j < n; j ++)
+            {
+                if (firstCheck)
+                {
+                    a *= a;
+                    firstCheck = false;
+                }
+                a *= a1;
+            }
+            return a;
+        }
+        static long PowerLogN(long a, long n)
+        {
+
+            long p = 1;
+            for(; n > 1; n /= 2)
+            {
+                if( n % 2 ==1)
+                {
+                    p *= a;
+                }
+                a *= a;
+            }
+            p *= a;
+            return p;
+        }
+    }
+}
